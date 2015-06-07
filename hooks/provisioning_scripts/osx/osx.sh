34f4fe157273e5be7d8059fab17253cffc8cd2ee
Muhaimin Ahsan <leroux@fezrev.com>
2015-06-06 19:05:34 -0500

    Install iTerm2 preferences.

--- hooks/provisioning_scripts/osx/osx.sh
+++ hooks/provisioning_scripts/osx/osx.sh
@@ -5,3 +5,8 @@ OSX_DIR="${BASH_SOURCE%/*}"
 source "$OSX_DIR/osx-defaults.sh"
 command -v xcodebuild >/dev/null 2>&1 || source "$OSX_DIR/xcode-cli-tools.sh"
 source "$OSX_DIR/setup-brew.sh"
+
+# Install iTerm preferences.
+defaults delete com.googlecode.iterm2
+cp $OSX_DIR/config/com.googlecode.iterm2.plist ~/Library/Preferences/
+defaults read -app iTerm2
