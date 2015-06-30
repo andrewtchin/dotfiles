b40b6e651c6e216e26c8b064e73a95af610d8a9b
Muhaimin Ahsan <leroux@fezrev.com>
2015-06-30 17:40:57 +0000

    Add gitconfig email-guess script.

---
+++ tag-git/git-scripts/email-guess.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+remote=`git remote -v | awk '/\(push\)$/ {print $2}'`
+email=leroux@fezrev.com # default
+
+if [[ $remote == *sourcecode.tvec* ]]; then
+  email=mahsan@vectranetworks.com
+fi
+
+echo "Configuring user.email as $email"
+git config user.email $email
