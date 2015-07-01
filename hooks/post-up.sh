21e6c060794c30ce91958fe9b951a703d79d11ff
Muhaimin Ahsan <leroux@fezrev.com>
2015-07-01 01:36:43 -0500

    Do vim post install last.

--- hooks/post-up.sh
+++ hooks/post-up.sh
@@ -5,6 +5,14 @@ set -o pipefail
 set -o nounset
 # set -o xtrace
 
+# Set zsh as default shell.
+if default_shell=$(command -v /bin/zsh); then
+    chsh -s $default_shell
+fi
+
+# Install crontab
+crontab ~/.crontab
+
 # Install vim bundles.
 if [ ! "$(ls -A $HOME/.vim/bundle >/dev/null 2>&1)" ]; then
   vim +BundleInstall +qa
@@ -15,7 +23,7 @@ fi
 #
 VIMPROC_DIR="$HOME/.vim/bundle/vimproc"
 if [ -d "$VIMPROC_DIR" ]; then
-    cd $VIMPROC_DIR; make>/dev/null; cd ~-
+    cd $VIMPROC_DIR; make>/dev/null || :; cd ~-
 fi
 
 YCM_DIR="$HOME/.vim/bundle/YouCompleteMe"
@@ -23,14 +31,6 @@ if [ -d "$YCM_DIR" ] &&
    [ ! $(ls -A $YCM_DIR/third_party/ycmd/ycm_core.* >/dev/null 2>&1) ]; then
     echo "Building YCM."
     cd $YCM_DIR
-    ./install.sh --clang-completer
+    ./install.sh --clang-completer || :
     cd ~-
-fi
-
-# Install crontab
-crontab ~/.crontab
-
-# Set zsh as default shell.
-if default_shell=$(command -v zsh); then
-    chsh -s $default_shell
 fi
