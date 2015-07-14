1405c9e62414a1007042f4ba36f172035a3e9719
Andrew Chin <andrewtchin@gmail.com>
2015-07-14 17:16:13 -0500

    Add install script.

---
+++ hooks/provisioning_scripts/osx/osx-single.sh
@@ -0,0 +1,3 @@
+curl -L https://raw.githubusercontent.com/andrewtchin/dotfiles/master/hooks/provisioning_scripts/osx/xcode-cli-tools.sh -o /tmp/xcode-cli-tools.sh
+chmod +x /tmp/xcode-cli-tools.sh
+/tmp/xcode-cli-tools.sh
