48da4883ed31f1cdb58dd283393ce7cd24b9e8a6
Andrew Chin <andrewtchin@gmail.com>
2015-07-14 17:31:00 -0500

    Update install script.

--- hooks/provisioning_scripts/osx/osx-single.sh
+++ hooks/provisioning_scripts/osx/osx-single.sh
@@ -1,3 +1,15 @@
 curl -L https://raw.githubusercontent.com/andrewtchin/dotfiles/master/hooks/provisioning_scripts/osx/xcode-cli-tools.sh -o /tmp/xcode-cli-tools.sh
 chmod +x /tmp/xcode-cli-tools.sh
 /tmp/xcode-cli-tools.sh
+
+# Install homebrew.
+# Insecure...yeah... (should expand to the contents of the script)
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" || true
+
+brew tap thoughtbot/formulae
+brew install rcm
+
+git clone https://github.com/andrewtchin/dotfiles-local ~/.dotfiles-local
+git clone https://github.com/andrewtchin/dotfiles ~/.dotfiles
+RCRC="~/.dotfiles/rcrc"
+~/.dotfiles/bin/rcup
