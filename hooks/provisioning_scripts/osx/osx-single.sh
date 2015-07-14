4767e677079b71d77fb58ae5b01d25f33b042ccc
Andrew Chin <andrewtchin@gmail.com>
2015-07-14 17:51:45 -0500

    Fix quotes.

--- hooks/provisioning_scripts/osx/osx-single.sh
+++ hooks/provisioning_scripts/osx/osx-single.sh
@@ -3,20 +3,20 @@ $DOTFILES_DIR="$HOME/.dotfiles"
 $DOTFILES_LOCAL_DIR="$HOME/.dotfiles-local"
 
 echo "\n\033[0;32m>>> Pre-install cleanup\033[0m"
-if [ -$XCODE_CLI_TOOLS ]; then
+if [ "$XCODE_CLI_TOOLS" ]; then
     rm -f $XCODE_CLI_TOOLS
 fi
-if [ -d $DOTFILES_DIR ]; then
+if [ -d "$DOTFILES_DIR" ]; then
     rm -rf $DOTFILES_DIR
 fi
-if [ -d $DOTFILES_LOCAL_DIR ]; then
+if [ -d "$DOTFILES_LOCAL_DIR" ]; then
     rm -rf $DOTFILES_LOCAL_DIR
 fi
 
 echo "\n\033[0;32m>>> Install xcode\033[0m"
 curl -L https://raw.githubusercontent.com/andrewtchin/dotfiles/master/hooks/provisioning_scripts/osx/xcode-cli-tools.sh -o $XCODE_CLI_TOOLS
-chmod +x $XCODE_CLI_TOOLS
-$XCODE_CLI_TOOLS
+chmod +x "$XCODE_CLI_TOOLS"
+"$XCODE_CLI_TOOLS"
 
 echo "\n\033[0;32m>>> Install homebrew\033[0m"
 # Insecure...yeah... (should expand to the contents of the script)
@@ -27,7 +27,7 @@ brew tap thoughtbot/formulae
 brew install rcm
 
 echo "\n\033[0;32m>>> Run rcm\033[0m"
-git clone https://github.com/andrewtchin/dotfiles-local $DOTFILES_LOCAL_DIR
-git clone https://github.com/andrewtchin/dotfiles $DOTFILES_DIR
+git clone https://github.com/andrewtchin/dotfiles-local "$DOTFILES_LOCAL_DIR"
+git clone https://github.com/andrewtchin/dotfiles "$DOTFILES_DIR"
 RCRC="$DOTFILES_DIR/rcrc"
-$DOTFILES_DIR/bin/rcup
+"$DOTFILES_DIR/bin/rcup"
