f786cc66ddceae43061faad0e44aabca934e89af
Andrew Chin <andrewtchin@gmail.com>
2015-08-30 19:00:08 -0500

    Remove OSX provisioning.

--- hooks/provision.sh
+++ hooks/provision.sh
@@ -9,7 +9,3 @@ __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 __root="$(cd "$(dirname "${__dir}")" && pwd)" # <-- change this
 __file="${__dir}/$(basename "${BASH_SOURCE[0]}")"
 __base="$(basename ${__file} .sh)"
-
-if [[ "$OSTYPE" == "darwin"* ]]; then
-    source "${__dir}/provisioning_scripts/osx/osx.sh"
-fi
