819f411e39674443a4af7380fdbb9353a83da040
Muhaimin Ahsan <leroux@fezrev.com>
2015-09-28 20:59:05 -0500

    Fix osx provisioning script.

--- hooks/provisioning_scripts/osx/osx.sh
+++ hooks/provisioning_scripts/osx/osx.sh
@@ -3,7 +3,7 @@
 set -o errexit
 set -o pipefail
 set -o nounset
-# set -o xtrace
+set -o xtrace
 
 __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 __root="$(cd "$(dirname "${__dir}")" && pwd)" # <-- change this
@@ -65,17 +65,6 @@ casks=(
     xquartz
 )
 
-main () {
-    source "${__dir}/osx-defaults.sh"
-    command -v xcodebuild >/dev/null 2>&1 || source "${__dir}/xcode-cli-tools.sh"
-    setup_brew "${taps}" "${pkgs}" "${casks}"
-
-    # Install Preferences (in the form of plists)
-    for plist in ${__dir}/*.plist; do
-        install_plist $plist
-    done
-}
-
 # setup_brew(taps, pkgs, casks)
 setup_brew () {
     (( $# != 4 )) || { echo "$0 requires 3 parameters to be passed to it."; exit 1; }
@@ -111,12 +100,23 @@ install_plist () {
     plist_filename="$(basename $plist_path)"
     plist_domain="${plist_filename%.*}"
 
-    defaults delete "${plist_domain}"
-    cp "${plist_path}" "~/Library/Preferences"
-    defaults read "${plist_domain}"
+    defaults delete "${plist_domain}" || :
+    cp "${plist_path}" ~/Library/Preferences/
+    defaults read "${plist_domain}" 2>1 >/dev/null
+}
+
+main () {
+    source "${__dir}/osx-defaults.sh"
+    command -v xcodebuild >/dev/null 2>&1 || source "${__dir}/xcode-cli-tools.sh"
+    setup_brew "${taps}" "${pkgs}" "${casks}"
+
+    # Install Preferences (in the form of plists)
+    for plist in ${__dir}/plists/*.plist; do
+        install_plist $plist
+    done
+
+    echo ""
+    echo "${red}Done. Reboot system as some changes only take effect on reboot.${reset}"
 }
 
 main "$@"
-
-echo ""
-echo "${red}Done. Reboot system as some changes only take effect on reboot.${reset}"
