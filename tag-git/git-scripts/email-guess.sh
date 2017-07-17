86ff86207b51ff715d240a462eef1afa3859aa6e
Andrew Chin <andrew@andrewtchin.com>
2017-07-17 08:40:12 -0700

    Update git email

--- tag-git/git-scripts/email-guess.sh
+++ tag-git/git-scripts/email-guess.sh
@@ -4,7 +4,7 @@ remote=`git remote -v | awk '/\(push\)$/ {print $2}'`
 email="git@andrewtchin.com" # default
 hostname=$(hostname -s)
 
-if [[ $remote == *github.com*vic.git ]]; then
+if [[ $remote == *github.com*vic.git ]] || [[ $remote == *github.com*vic-product.git ]]; then
     email="andrewchin@vmware.com"
 fi
 
